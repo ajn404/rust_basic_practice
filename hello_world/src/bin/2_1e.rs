955d81248ca4f8f4f70152ecd63ebb2a8b48b063
ajn404 <huiyuening00@gmail.com>
2022-11-17 16:22:17 +0800

    Submit:

---
+++ hello_world/src/bin/2_1e.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = 5;
+    // 在main函数的作用域内对之前的x进行遮蔽
+    let x = x + 1;
+    {
+        // 在当前的花括号作用域内，对之前的x进行遮蔽
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+    println!("The value of x is: {}", x);
+}
