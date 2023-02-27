d859f66d8d028f871fa17c084bf410ed3805f88f
mac_ajn404 <huiyuening00@gmail.com>
2023-02-27 17:54:52 +0800

    no message

--- helloRust2_4_3/src/lib.rs
+++ helloRust2_4_3/src/lib.rs
@@ -1,5 +1,3 @@
-
-
 struct User {
     name: String,
     mobile: String,
@@ -8,14 +6,12 @@ struct User {
     des: String,
 }
 
-
 #[derive(Debug)]
 
 struct File {
     name: String,
     data: Vec<u8>,
 }
-
 
 #[cfg(test)]
 mod test_struct {
@@ -104,6 +100,50 @@ mod test_struct {
         println!("{} is {} bytes long", f1_name, f1_length);
     }
 
-    
+    #[test]
+    fn tupleStruct() {
+        struct Color(i32, i32, i32);
+        let c = Color(12, 22, 33);
+        dbg!(c.0);
+    }
 
+    #[test]
+    fn unit_like_struct() {
+        #[derive(Debug)]
+        pub struct AlwaysEqual;
+        pub trait haiha {
+            //这是因为在Rust中，特质(trait)方法必须被定义为实例方法(associated method)，也就是说它们必须有一个参数，该参数表示方法所属的类型。这个参数称为Self，它可以是具体类型、引用、借用等形式。
+            fn mma(&self) {}
+        }
+        impl haiha for AlwaysEqual {
+            fn mma(&self) {
+                dbg!("hahah");
+            }
+        }
+        let a = AlwaysEqual;
+        println!("www{:?}", a);
+        a.mma();
+    }
+
+
+    #[test]
+    fn about_struct_04(){
+        #[derive(Debug)]
+        // /该生命周期标注说明，结构体 所引用的字符串 str 必须比该结构体活得更久
+        struct User<'a>{
+            username:&'a str,
+            email:&'a str,
+            sign_in_count:u64,
+            active:bool,
+        }
+
+        let user = User{
+            username:"hh@hh.com",
+            email:"wwww",
+            sign_in_count:32,
+            active:false,
+        };
+        dbg!(user);
+
+    }
 }
